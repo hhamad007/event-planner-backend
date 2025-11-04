9f267fbeda4f052e2b59e5b14216bf30ae56679a
Hamad Hussain <hhtech007@proton.me>
2025-11-04 19:56:41 +0000

    databse-models-implementations

--- models/User.js
+++ models/User.js
@@ -1,1 +1,87 @@
-/* Mongoose Models */
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [2, "Name must be at least 2 characters long"],
+      maxlength: [50, "Name cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      select: false, // Don't return password in queries by default
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    phone: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return !v || validator.isMobilePhone(v);
+        },
+        message: "Please provide a valid phone number",
+      },
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    lastLogin: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Index for faster queries
+userSchema.index({ email: 1 });
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  // Only hash password if it's modified
+  if (!this.isModified("password")) return next();
+
+  try {
+    // Hash password with salt rounds of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Remove password from JSON output
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+  delete userObject.password;
+  return userObject;
+};
+
+module.exports = mongoose.model("User", userSchema);
